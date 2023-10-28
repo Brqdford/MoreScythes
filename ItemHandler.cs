8621f0bb70001f8d3f623418bd426c803c11dff3
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-10-28 17:42:22 +1100

    Updated for 1.3
    Updated for 1.3
    Corrected Scythe ordering

--- ItemHandler.cs
+++ ItemHandler.cs
@@ -1,106 +1,91 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Morthy.Util;
 using UnityEngine;
 using Wish;
 using Object = UnityEngine.Object;
-using HarmonyLib;
+using System.Reflection;
+
 namespace MoreScythes;
 
 public static class ItemHandler
 {
     private const int OriginalScytheId = 3000;
     private const int AdamantScytheId = 30000;
-    private const int MithrilScytheId = 30090;
-    private const int SuniteScytheId = 30020;
-    private const int GloriteScytheId = 30030;
+    private const int MithrilScytheId = 30020;
+    private const int SuniteScytheId = 30030;
+    private const int GloriteScytheId = 30090;
+    private const string RecipeListAnvil = "RecipeList_Anvil";
 
-    private static void CreateScytheItem(int id, int speed, int damage)
+    private static void CreateAndConfigureItem(int id, int speed, int damage)
     {
-
         var original = ItemDatabase.GetItemData(OriginalScytheId);
 
-        var dmgx = damage - 8;
-        var dmgy = damage;
         var item = ScriptableObject.CreateInstance<ItemData>();
-        JsonUtility.FromJsonOverwrite(FileLoader.LoadFile(Assembly.GetExecutingAssembly(), $"data.{id}.json"), item);
-        item.icon = SpriteUtil.CreateSprite(FileLoader.LoadFileBytes(Assembly.GetExecutingAssembly(), $"img.{id}.png"), $"Modded item icon {id}");
+        JsonUtility.FromJsonOverwrite(
+            FileLoader.LoadFile(Assembly.GetExecutingAssembly(), $"data.{id}.json"), 
+            item);
+
+        item.icon = SpriteUtil.CreateSprite(
+            FileLoader.LoadFileBytes(Assembly.GetExecutingAssembly(), $"img.{id}.png"), 
+            $"Modded item icon {id}");
 
         var useItem = Object.Instantiate(original.useItem);
         if (!useItem)
         {
-            Plugin.logger.LogError("Original scythe has no useItem");
+            MainPlugin.Log.LogError("Original scythe has no useItem");
             return;
         }
+
         item.useItem = useItem;
-
-        var frameRate = useItem.gameObject.GetComponent<Weapon>();
-        Traverse.Create(frameRate).Field("_frameRate").SetValue(speed);
-
-        useItem.gameObject.GetComponent<DamageSource>()._damageRange.Set(dmgx, dmgy);
-
+        useItem.gameObject.GetComponent<Weapon>()._frameRate = speed;
+        useItem.gameObject.GetComponent<DamageSource>()._damageRange.Set(damage - 8, damage);
         useItem.gameObject.SetActive(false);
 
         Object.DontDestroyOnLoad(useItem);
         ItemDatabase.items[item.id] = item;
         ItemDatabase.ids[item.name.RemoveWhitespace().ToLower()] = item.id;
-        Plugin.logger.LogDebug($"Created item {item.id} with name {item.name}");
-    }
-    private static void AddItemToRecipeList(int id, string recipeList, List<ItemInfo> input)
-    {
+        ItemDatabase.itemDatas[item.id] = item;
 
+        MainPlugin.Log.LogWarning($"Created item {item.id} with name {item.name}");
+    }
+
+    private static void ConfigureRecipe(int itemId, string recipeListName, List<ItemInfo> inputs, float craftingHours)
+    {
         foreach (var rl in Resources.FindObjectsOfTypeAll<RecipeList>())
         {
-            if (!rl.name.Equals(recipeList)) continue;
-            
-            if (rl.craftingRecipes.Any(r => r.output.item.id == id))
+            if (!rl.name.Equals(recipeListName)) continue;
+
+            if (rl.craftingRecipes.Exists(r => r.output.item.id == itemId))
             {
                 return;
             }
+
             var recipe = ScriptableObject.CreateInstance<Recipe>();
-            recipe.output = new ItemInfo { item = ItemDatabase.GetItemData(id), amount = 1 };
-            recipe.input = input;
+            recipe.output = new ItemInfo { item = ItemDatabase.GetItemData(itemId), amount = 1 };
+            recipe.input = inputs;
             recipe.worldProgressTokens = new List<Progress>();
             recipe.characterProgressTokens = new List<Progress>();
             recipe.questProgressTokens = new List<QuestAsset>();
-            if (id==30000){
-            recipe.hoursToCraft = 6f;
-            }else if (id==30090){
-            recipe.hoursToCraft = 12f;
-            }else if (id == 30020){
-            recipe.hoursToCraft = 18f;
-            }else {
-            recipe.hoursToCraft = 24f;
-            }
+            recipe.hoursToCraft = craftingHours;
 
             rl.craftingRecipes.Add(recipe);
-            Plugin.logger.LogDebug($"Added item {id} to {recipeList}");
+            MainPlugin.Log.LogWarning($"Added item {itemId} to {recipeListName}");
         }
     }
 
     public static void CreateScytheItems()
     {
-        CreateScytheItem(AdamantScytheId, 13, 14);
-        CreateScytheItem(MithrilScytheId, 14, 18);
-        CreateScytheItem(SuniteScytheId, 15, 22);
-        CreateScytheItem(GloriteScytheId, 16, 26);
-        AddItemToRecipeList(30000, "RecipeList_Anvil", new List<ItemInfo>
+        var scytheDefinitions = new List<(int id, int speed, int damage, string recipeList, List<ItemInfo> inputs, float craftingHours)>
         {
-            new() { item = ItemDatabase.GetItemData(ItemID.AdamantBar), amount = 10 }
-        });
-        AddItemToRecipeList(30090, "RecipeList_Anvil", new List<ItemInfo>
+            (AdamantScytheId, 13, 14, RecipeListAnvil, new List<ItemInfo> { new() { item = ItemDatabase.GetItemData(ItemID.AdamantBar), amount = 10 } }, 6f),
+            (MithrilScytheId, 14, 18, RecipeListAnvil, new List<ItemInfo> { new() { item = ItemDatabase.GetItemData(ItemID.MithrilBar), amount = 10 } }, 12f),
+            (SuniteScytheId, 15, 22, RecipeListAnvil, new List<ItemInfo> { new() { item = ItemDatabase.GetItemData(ItemID.SuniteBar), amount = 10 } }, 24f),
+            (GloriteScytheId, 16, 26, RecipeListAnvil, new List<ItemInfo> { new() { item = ItemDatabase.GetItemData(ItemID.GloriteBar), amount = 10 } }, 48f),
+        };
+
+        foreach (var (id, speed, damage, recipeList, inputs, craftingHours) in scytheDefinitions)
         {
-            new() { item = ItemDatabase.GetItemData(ItemID.MithrilBar), amount = 10 }
-        });
-        
-        AddItemToRecipeList(30020, "RecipeList_Anvil", new List<ItemInfo>
-        {
-            new() { item = ItemDatabase.GetItemData(ItemID.SuniteBar), amount = 10 }
-        });
-        AddItemToRecipeList(30030, "RecipeList_Monster Anvil", new List<ItemInfo>
-        {
-            new() { item = ItemDatabase.GetItemData(ItemID.GloriteBar), amount = 10 }
-        });
+            CreateAndConfigureItem(id, speed, damage);
+            ConfigureRecipe(id, recipeList, inputs, craftingHours);
+        }
     }
 }
---
+++ MainPlugin.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using Wish;
+
+namespace MoreScythes;
+
+[BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+public class MainPlugin : BaseUnityPlugin
+{
+    private const string PluginGuid = "org.brqdfordsdevelopmentgroup.morescythes";
+    private const string PluginName = "More Scythes";
+    private const string PluginVersion = "1.0.9";
+    public static ManualLogSource Log { get; private set; }
+
+    private void Awake()
+    {
+        Log = Logger;
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginGuid);
+        Logger.LogInfo("More Scythes plugin has loaded successfully.");
+    }
+
+
+}
